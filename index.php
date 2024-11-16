2a8446478cd627b11e17feb4d738b9484ad501c3
akash6rout <akash06.rout@gmail.com>
2024-11-16 18:57:36 +0530

    FirstPhpCode

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+//echo "Hello World";
+// $first =15;
+// echo $first;
+// $second = "akash";
+// $third = "kumar";
+// echo $second.$third."HELLO!"."1";
+
+    // $a=10;
+    // $b=3;
+    // $d=5;
+    // $c=$a+$b;
+    // echo "\nadd:\t".$c;
+    // $c=$a-$b;
+    // echo "\nsubt:\t".$c;
+    // $c=$a*$b;
+    // echo "\n mult:\t".$c;
+    // $c=$a%$b;
+    // echo "\n mod:\t".$c;
+    // $d++;
+    // echo "increment:".$d;
+
+    // $x=5;
+    // $y=5;
+
+    // var_dump($x==$y);
+
+    // $a=1;
+    // $b=0;
+    // $c=$a && $b;
+    // var_dump($c);
+
+    // $x=5;
+    // $x > 7 ? $flag ="Yes": $flag ="No";
+    // var_dump($flag);
+
+    $a=5;
+    $b=6;
+    echo $a."\n";
+    $a+=$b;
+    echo $a."\n";
+?>
