729da90af5f619c451e7a188d04b660615a0607f
Akash Kumar Rout <akash06.rout@gmail.com>
2024-12-23 11:26:47 +0530

    Add new file

--- index.php
+++ index.php
@@ -34,9 +34,73 @@
     // $x > 7 ? $flag ="Yes": $flag ="No";
     // var_dump($flag);
 
-    $a=5;
-    $b=6;
-    echo $a."\n";
-    $a+=$b;
-    echo $a."\n";
+    // $a=5;
+    // $b=6;
+    // echo $a."\n";
+    // $a+=$b;
+    // echo $a."\n";
+
+    // $first = 800;
+    // echo $first;
+
+    // $firstName = "WsCube";
+    // $lastName = " Tech";
+    // echo $firstName.$lastName;
+
+    // echo $fullName = "Akash Kumar Rout";
+
+    // $age = 10;
+    //  
+    // if($age >= 18){
+        // echo "Yes You Can Vote";
+    // }else{
+        // echo "No You Can Not";
+    // }
+
+    // $age = 17;
+
+    // if($age>=18){
+        // echo "Yes you can vote";
+    // }elseif($age == 17){
+        // echo "Please wait one more year";
+    // }else{
+        // echo "You Cannot vote";
+    // }
+
+    //  NESTED IF - ELSE
+    // $age =67;
+
+    // if($age>=18){
+        // if($age<66){
+            // echo "Yes you can vote";
+        // }else{
+            // echo "You cannot vote anymore";
+        // }
+    // }else{
+        // echo "you cannot vote";
+    // }
+
+    // INTRODUCTION TO ARRAY
+        //index array
+        // $arr1 = array("Car1","Car2","Car3");
+        // $arr1[3]="Car4";
+
+        //associate array
+        // $arr2 = array(
+            // 'name'=> 'akah kumar rout',
+            // 'company'=> 'akash trading',
+            // 'contact'=> array("9668345131","7008606368"),
+            // 'age'=> 21,
+            // 'status'=> true,
+        // );
+
+        // $arr2['test']= "trading";
+    
+        // echo "<pre>";
+        // print_r($arr2);
+        // echo "</pre>";
+
+        //function => User defined functions Pre defined functions
+        
+
 ?>
