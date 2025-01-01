355029fbaff515448dd57e046ff6b8b93da2bd74
Akash Kumar Rout <akash06.rout@gmail.com>
2025-01-01 17:38:42 +0530

    function

---
+++ Loop.php
@@ -0,0 +1,11 @@
+<?php
+//10 times hello
+$i = 0 ;
+while($i<10){
+    //echo "Hello <br/>";
+    echo $i."<br/>";
+    $i++;
+}
+echo "Loops Ends";
+
+?>
--- function.php
+++ function.php
@@ -2,5 +2,42 @@
 
 //going to practice function 
 //functions
+function p($data){
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
 
-?>
+$x = array(
+    "Car1",
+    "Car2",
+    "Car3",
+    "Car4",
+    "Car5",
+    "Car6",
+);
+
+p($x);
+
+$y = array(
+    "Car7",
+    "Car8",
+    "Car9",
+    "Car10",
+    "Car11",
+    "Car12",
+);
+
+p($y);
+
+function vote($age){
+    if($age >=18 && $age <=65){
+        return "Yes you can vote";
+    }else{
+        return "No you can not vote";
+    }
+}
+
+echo vote(24);
+
+?> 
