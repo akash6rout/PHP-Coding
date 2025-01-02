ce4faf62aa73e6fd7a7368e65ef680f4e3628fee
Akash Kumar Rout <akash06.rout@gmail.com>
2025-01-02 18:05:32 +0530

    for loop

--- Loop.php
+++ Loop.php
@@ -1,11 +1,17 @@
 <?php
 //10 times hello
-$i = 0 ;
-while($i<10){
+// $i = 0 ;
+// while($i<10){
     //echo "Hello <br/>";
-    echo $i."<br/>";
-    $i++;
+    // echo $i."<br/>";
+    // $i++;
+// }
+// echo "Loops Ends";
+
+
+for($j=10; $j>0; $j--){
+    echo $j."<br/>";
 }
-echo "Loops Ends";
+
 
 ?>
